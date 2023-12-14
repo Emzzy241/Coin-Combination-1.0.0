3181035cb0b1daa5d748a1235f9a14810f0b5042
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-14 13:18:51 +0100

    new update

--- CoinCombos/Program.cs
+++ CoinCombos/Program.cs
@@ -14,36 +14,47 @@ namespace CoinCombos.Models
             Console.WriteLine("Welcome to the Coin combinations application");
             Console.WriteLine();
             Console.WriteLine("Enter an amount in cents:");
-            int amount = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine();
-            Coins userCoin = new Coins(0, 0, 0, 0);
-            userCoin.CalculateCoinCombinationsRecursive(amount);
-            Console.WriteLine("Your result is: ");
-            Console.WriteLine("Quarters: " + userCoin.Quarter + "  Dimes: " + userCoin.Dime + "  Nickels: " + userCoin.Nickel + "  Pennies: " + userCoin.Penny);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Do you need to enter another amount? (Y/N)");
-            string userInput = Console.ReadLine().ToUpper();
-            if(userInput == "Y")
+            try
             {
+                int amount = int.Parse(Console.ReadLine());
                 Console.WriteLine();
                 Console.WriteLine();
-                Main();
-            }
-            else if (userInput == "N")
-            {
+                Coins userCoin = new Coins(0, 0, 0, 0);
+                userCoin.CalculateCoinCombinationsRecursive(amount);
+                Console.WriteLine("Your result is: ");
+                Console.WriteLine("Quarters: " + userCoin.Quarter + "  Dimes: " + userCoin.Dime + "  Nickels: " + userCoin.Nickel + "  Pennies: " + userCoin.Penny);
                 Console.WriteLine();
                 Console.WriteLine();
-                Console.WriteLine("Goodbye");
+                Console.WriteLine("Do you need to enter another amount? (Y/N)");
+                string userInput = Console.ReadLine().ToUpper();
+                if(userInput == "Y")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Main();
+                }
+                else if (userInput == "N")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Goodbye");
 
+                }
+                else
+                {
+                    Console.WriteLine("Sorry I didn't get that, Please try again");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Main();
+                }
             }
-            else
+            catch (System.Exception ex)
             {
-                Console.WriteLine("Sorry I didn't get that, Please try again");
-                Console.WriteLine();
-                Console.WriteLine();
-                Main();
+                Console.WriteLine("Message = {0}", ex.Message);
+                Console.WriteLine("Source = {0}", ex.Source);
+                Console.WriteLine("StackTrace = {0}", ex.StackTrace);
+                Console.WriteLine("TargetSite = {0}", ex.TargetSite);
+                Console.WriteLine("Please e in numbers only");
             }
         }
     }
